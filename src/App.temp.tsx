eb69f2d9e2150baab0eba5c1bd677749e23655d0
jun-uen0 <hello@junueno.dev>
2022-05-21 17:59:09 +0900

    added まゆしぃ

--- src/App.temp.tsx
+++ src/App.temp.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import { useState } from "react";
 import logo from './logo.svg';
 import kei from './kei.jpg';
 import banana from './banana.png';
 import './App.css';
+import useSound from 'use-sound';
+import Sound from './mayu.mp3';
 
 const newArr = new Array(10).fill('バナナ')
 console.log('newArr', newArr)
 
 function App() {
+  const [play, { stop, pause }] = useSound(Sound);
   return (
     <div className="App">
       <header className="App-header">
         <img src={kei} className="App-logo" alt="logo" />
+        <button onClick={() => play()}>トゥットゥルー</button>
         <p>
           バナナ研究家藤野圭一によるクソサイト
         </p>
