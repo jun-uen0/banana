18f6a5d3fd6c0b4278077c8e285ff5fff4f5cd11
jun-uen0 <hello@junueno.dev>
2022-05-22 15:08:48 +0900

    changed some

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App.temp';
+import kei from './image/kei.jpg';
+import banana from './image/banana.png';
+import './App.css';
+import useSound from 'use-sound';
+import Sound from './music/mayu.mp3';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+function App() {
+  const [play] = useSound(Sound);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          バナナ研究家藤野圭一によるクソサイト
+        </p>
+        <div>
+        <img src={kei} className="App-logo" alt="logo" />
+        <img src={banana} className="App-logo" alt="logo" />
+        </div>
+        
+        <div className="App-list">
+          <p>みる</p>
+          <li>クラナド</li>
+          <li>Air</li>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
