5e2c2716f0a7e7e523d415aef437826df488a751
jun-uen0 <hello@junueno.dev>
2022-05-25 13:39:32 +0900

    added camp english onsen

---
+++ src/components/Camp.tsx
@@ -0,0 +1,8 @@
+export const Camp = () => {
+  return (
+    <>
+      <p>キャンプ</p>
+      <li>キャンプしよ</li>
+    </>
+  );
+}
---
+++ src/components/EnglishStudy.tsx
@@ -0,0 +1,8 @@
+export const EnglishStudy = () => {
+  return (
+    <>
+      <p>英語学習</p>
+      <li>とりまTOEIC受ける</li>
+    </>
+  );
+}
